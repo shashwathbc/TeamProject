75d4cfa02240b72f430855e7a06915fee93419c3
Shashwath <shashwathbc@gmail.com>
2022-06-01 14:25:34 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { React } from 'react';
+import AboutPage from './pages/AboutScreen/AboutPage';
+import HomePage from './pages/HomeScreen/HomePage';
 
 function App() {
   return (
     <div className="App">
       <h1>WELCOME</h1>
+      <HomePage/>
+      <AboutPage/>
     </div>
   );
 }
